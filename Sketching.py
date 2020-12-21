1902b6b79d7fdeccc27668561fe30212edff6786
sakshiagarwal439 <70151005+sakshiagarwal439@users.noreply.github.com>
2020-12-21 19:09:00 +0530

    Add files via upload

---
+++ Sketching.py
@@ -0,0 +1,19 @@
+import cv2
+
+def dodgeV2(x, y):
+    return cv2.divide(x, 255-y, scale=256)
+
+
+img_bgr = cv2.imread('photo.jpg')
+img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
+cv2.imshow("Image", img_bgr)
+
+img_invert = cv2.bitwise_not(img_gray)
+
+img_smooth = cv2.GaussianBlur(img_invert, (21, 21), sigmaX = 0, sigmaY = 0)
+
+final_img = dodgeV2(img_gray, img_smooth)
+cv2.imshow("Final Image", final_img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
